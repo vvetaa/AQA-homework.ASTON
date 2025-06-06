d259435db896dfbe6162a5d5a4a6fc92047ec8ac
vvetaa <s.seredina99@mail.ru>
2025-06-06 22:41:33 +0300

    Выполнение ДЗ к уроку 2_6

--- src/main/java/lesson2_6/task_2/Main.java
+++ src/main/java/lesson2_6/task_2/Main.java
@@ -1,4 +1,14 @@
 package lesson2_6.task_2;
 
 public class Main {
+    public static void main(String[] args) {
+        PhoneDirectory directory = new PhoneDirectory();
+
+        directory.add("Иванов", "+798521431174");
+        directory.add("Иванов", "+798521432274");
+        directory.add("Петров", "+798521432255");
+
+        System.out.println("Номера по фамилии Иванов: " + directory.get("Иванов"));
+        System.out.println("Номера по фамилии Петров: " + directory.get("Петров"));
+    }
 }
